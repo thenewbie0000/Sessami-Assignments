865dbce458dae9a292ec2cc4ea1f839bc1f9d2d3
Rojesh Shrestha <shrestharojesh34@gmail.com>
2023-11-02 12:22:34 +0545

    Update script.js

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -2,7 +2,7 @@ const inputBox = document.getElementById("input-box");
 const  listContainer= document.getElementById("list-container");
 
 function addTask(){
-  if(inputBox.value === ''){
+  if(inputBox.value.trim() === ''){
     alert('First write the note!');
   }
   else{
