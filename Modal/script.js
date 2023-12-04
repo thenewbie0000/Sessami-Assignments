2b9d36f9e15c05a4ae4ba0fa6927fa62f09eabdd
Rojesh Shrestha <shrestharojesh34@gmail.com>
2023-12-04 19:01:55 +0545

    Updated Slide Out effect

--- Modal/script.js
+++ Modal/script.js
@@ -1,15 +1,40 @@
-const modal = document.querySelector('.modal');
-const openButton = document.querySelector('.btn');
-const closeButton = document.querySelector('.close');
+const modal = document.querySelector(".modal");
+const openButton = document.querySelector(".btn");
+const closeButton = document.querySelector(".close");
+function showModal() {
+  modal.style.display = "flex";
+  modal.classList.remove("slide-out");
+}
 
-openButton.addEventListener('click',()=>{
-    modal.style.display = 'block';
+function hideModal() {
+  modal.classList.add("slide-out");
+  modal.addEventListener(
+    "animationend",
+    function () {
+      modal.style.display = "none";
+      modal.classList.remove("slide-out");
+    },
+    { once: true }
+  );
+}
+
+openButton.addEventListener("click", () => {
+  showModal();
 });
-closeButton.addEventListener('click',()=>{
-    modal.style.display = 'none';
+
+
+closeButton.addEventListener("click", () => {
+  hideModal();
 });
+
 document.addEventListener("keydown", (event) => {
-    if ((event.key === ' ' || event.key === 'Spacebar' || event.key === "Escape" || event.key ==="Enter") && modal.style.display === 'block'){
-        modal.style.display = 'none';
-    }
+  if (
+    (event.key === " " ||
+      event.key === "Spacebar" ||
+      event.key === "Escape" ||
+      event.key === "Enter") &&
+    modal.style.display === "flex"
+  ) {
+    hideModal();
+  }
 });
